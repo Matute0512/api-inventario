0de64631fcb278513f14bdc30cb0671bcd4260c1
Matute0512 <matiast999@hotmail.com>
2026-05-27 19:21:56 -0300

    feat: implement DTO pattern using Java Records for product
    - Se implementó el patrón DTO para la entidad Product.
    - Se creó ProductRequestDTO para encapsular los datos de entrada con validaciones (jakarta.validation).
    - Se creó ProductResponseDTO para estructurar los datos de salida.
    - Se utilizaron Java Records para garantizar inmutabilidad y reducir código boilerplate (KISS).

---
+++ src/main/java/io/github/torres/dto/ProductRequestDTO.java
@@ -0,0 +1,28 @@
+package io.github.torres.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.PositiveOrZero;
+
+import java.math.BigDecimal;
+
+public record ProductRequestDTO(
+
+    @NotBlank(message = "El nombre del producto es obligatorio")
+    @Size(max = 100, message = "El nombre no puede exeder los 100 caracteres")
+    String name,
+
+    @Size(max = 225, message = "La descripción no puede exceder los 255 caracteres")
+    String description,
+
+    @NotNull(message = "El precio es obligatorio")
+    @Positive(message = "El precio debe ser mayor a cero")
+    BigDecimal price,
+
+    @NotNull(message = "El stock es obligatorio")
+    @PositiveOrZero(message = "El stock no puede ser negativo")
+    Integer stock
+){}
+
